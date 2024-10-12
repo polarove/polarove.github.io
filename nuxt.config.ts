b32515c09159e694296afa7cac10daac66f56d5b
liuqi <liuqi6602@163.com>
2024-10-12 13:52:09 +0800

    css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	modules: ['@nuxt/icon', '@unocss/nuxt']
-	// usage: https://unocss.dev/integrations/nuxt#configuration
-	// unocss: {
-	// 	nuxtLayers: true
-	// }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  css: ["assets/css/normalize.css", "assets/css/scrollbar.css"],
+  modules: ["@nuxt/icon", "@unocss/nuxt"],
+  // usage: https://unocss.dev/integrations/nuxt#configuration
+  // unocss: {
+  // 	nuxtLayers: true
+  // }
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>hi</template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const d: number = 1;
+if (d == 2) {
+  if (d > 2) {
+    if (d < 2 || (d as number) == 5) {
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped></style>
