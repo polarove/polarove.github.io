76923d762c386e5133272ba883dde343d4674aaf
polarove <polarove@users.noreply.github.com>
2024-10-12 02:53:03 +0000

    deploy: 082c48a8a88fbe7140f60bf86c1d2ffd6dfedf4c

---
+++ _nuxt/D-S5stOv.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./B4mfBmzk.js";const r={};function c(n,t){return"hi"}const o=e(r,[["render",c]]);export{o as default};
--- pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>hi</template>
-
-<script lang="ts" setup></script>
-
-<style lang="scss" scoped></style>
