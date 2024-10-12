14f4fb7f4696ed2724a87295cb9f959fa98ed6ee
polarove <polarove@users.noreply.github.com>
2024-10-12 05:53:49 +0000

    deploy: b32515c09159e694296afa7cac10daac66f56d5b

--- _nuxt/22AS5pC5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlocaGEe.js";const r={};function c(n,t){return"hi"}const o=e(r,[["render",c]]);export{o as default};
---
+++ _nuxt/BB5fTlhs.js
@@ -0,0 +1,1 @@
+import{d as e}from"./FRQGz40P.js";const o=e({__name:"index",setup(n){return(t,r)=>"hi"}});export{o as default};
