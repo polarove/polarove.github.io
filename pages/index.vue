62300f352ee3e99fcfd9081e008d8a96c885ce03
liuqi <liuqi6602@163.com>
2024-10-14 16:51:26 +0800

    fs

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,8 @@
     <section class="flex flex-col items-center justify-center h-100vh">
         <client-only>
             <v-theme />
-            <p class="text-12">
-                {{ $colorMode.value }}
-            </p>
+            <p class="text-12">颜色模式： {{ $colorMode.value }}</p>
+            <p class="text-12">主题偏好： {{ $colorMode.preference }}</p>
         </client-only>
     </section>
 </template>
