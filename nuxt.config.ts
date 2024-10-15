59c80d3f768d8d095c5fa2c4d7597544efb80d16
liuqi <liuqi6602@163.com>
2024-10-15 14:50:56 +0800

    使用小米官网字体

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,16 @@ export default defineNuxtConfig({
         defaultLocale: zh_CN.code,
         translationDir: 'locales',
         meta: true
+    },
+    app: {
+        head: {
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://font.sec.miui.com/font/css?family=MiSans:300,400,500,600,700:Chinese_Simplify,Chinese_Traditional,Latin&display=swap'
+                }
+            ]
+        }
     }
 
     // usage: https://unocss.dev/integrations/nuxt#configuration
