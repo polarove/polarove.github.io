e0b2c643633195545082c225f32b18d8a6e1d492
刘棋 <liuqi6602@163.com>
2025-09-19 11:55:45 +0800

    update

--- components/v-action-bar/theme.vue
+++ components/v-action-bar/theme.vue
@@ -9,16 +9,15 @@
 				:name="theme.icon"
 				class="sm:text-4xl xs:text-3xl lt-xs:text-3xl cursor-pointer hover:color-$primary"
 				:class="{
-					'color-$primary': preference === theme.name
+					'color-$primary': $colorMode.preference === theme.name
 				}"
-				@click="preference = theme.name"
+				@click="$colorMode.preference = theme.name"
 			/>
 		</v-action-bar-item>
 	</v-action-bar>
 </template>
 
 <script lang="ts" setup>
-const { preference } = useColorMode()
 const themes = [
 	{
 		name: 'system',
